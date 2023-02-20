eeb0e17ab72050dc8fd570c2e934e1dce4e462e5
lucasartaud <lucas.artaud@gmail.com>
2023-02-20 23:41:35 +0100

    Scrape data

---
+++ server/eshops/circlesportswear.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.grid__item .card-wrapper.underline-links-hover')
+    .map((i, element) => {
+      const name = $(element)
+        .find('.full-unstyled-link')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      const price =
+        $(element)
+          .find('.money')
+          .text();
+
+      return {name, price};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
+    }
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const circlesportswear = require('./eshops/circlesportswear');
+const montlimart = require('./eshops/montlimart');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+/*async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/all-men') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
@@ -14,7 +16,37 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
     console.error(e);
     process.exit(1);
   }
+}*/
+
+async function sandbox (eshop = 'https://shop.circlesportswear.com/collections/all') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await circlesportswear.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
+
+/*async function sandbox (eshop = 'https://www.montlimart.com/99-vetements') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await montlimart.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}*/
 
 const [,, eshop] = process.argv;
 
