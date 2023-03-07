a5fceec9741ffd05a917b65c7637c2fe03cc3741
lucasartaud <lucas.artaud@gmail.com>
2023-03-07 15:36:45 +0100

    Update mongo.js

--- server/mongo.js
+++ server/mongo.js
@@ -15,7 +15,7 @@ async function insertProducts() {
     
     console.log(result);
 
-    client.close();
+    await client.close();
 }
 
 async function findProductsByBrand(brandName) {
@@ -28,8 +28,43 @@ async function findProductsByBrand(brandName) {
     console.log(`${result.length} products found for brand '${brandName}'`);
     console.log(result);
     
-    client.close();
+    await client.close();
 }
 
-//insertProducts();
-findProductsByBrand('Circle Sportswear');
+async function findProductsMaximumPrice(maximumPrice) {
+    const client = await MongoClient.connect(MONGODB_URI, { 'useNewUrlParser': true});
+    const db = client.db('clearfashion');
+
+    const collection = db.collection('products');
+    
+    const result = await collection.find({ price: { $lt: maximumPrice } }).toArray();
+    
+    console.log(`${result.length} products found for maximum price '${maximumPrice}'`);
+    console.log(result);
+    
+    await client.close();
+}
+
+async function findProductsSortedByPrice(order) {
+    const client = await MongoClient.connect(MONGODB_URI, { 'useNewUrlParser': true});
+    const db = client.db('clearfashion');
+
+    const collection = db.collection('products');
+    
+    const result = await collection.find().sort({ price: order }).toArray();
+    
+    console.log(`${result.length} products sorted by '${order}' (1 for ascending or -1 for descending order)`);
+    console.log(result);
+    
+    await client.close();
+}
+
+async function main() {
+    await insertProducts();
+    findProductsByBrand('Circle Sportswear');
+    findProductsMaximumPrice(30);
+    findProductsSortedByPrice(1);
+    findProductsSortedByPrice(-1);
+}
+
+main();
