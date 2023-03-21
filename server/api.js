babb047d1d7ad4ba775f4fa76e4243902a51490c
lucasartaud <lucas.artaud@gmail.com>
2023-03-21 15:24:32 +0100

    Update api.js

--- server/api.js
+++ server/api.js
@@ -25,14 +25,6 @@ app.listen(PORT);
 
 console.log(`📡 Running on port ${PORT}`);
 
-async function findProductById(productId) {
-  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-  const db = client.db(MONGODB_DB_NAME);
-  const collection = db.collection('products');
-  let result = await collection.find({ _id: ObjectId(productId) }).toArray();
-  return result;
-}
-
 app.get('/products/search', async (req, res) => {
   const limit = parseInt(req.query.limit) || 12;
   const brand = req.query.brand;
@@ -55,19 +47,34 @@ app.get('/products/search', async (req, res) => {
   res.json(result);
 });
 
+app.get('/products/:id', async (req, res) => {
+  const productId = req.params.id;
+
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db = client.db(MONGODB_DB_NAME);
+  const collection = db.collection('products');
+
+  const result = await collection.find({ _id: ObjectId(productId) }).toArray();
+
+  res.json(result);
+});
+
+app.get('/products', async (req, res) => {
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db = client.db(MONGODB_DB_NAME);
+  const collection = db.collection('products');
+
+  const result = await collection.find({}).toArray();
+
+  res.json(result);
+});
+
 app.get('/brands', async (req, res) => {
   const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
   const db = client.db(MONGODB_DB_NAME);
   const collection = db.collection('products');
 
-  const result = await collection.find();
-  print(result)
+  const result = await collection.distinct('brand');
 
   res.json(result);
-});
-
-app.get('/products/:id', async (req, res) => {
-  const productId = req.params.id;
-  const product = await findProductById(productId);
-  res.json(product);
 });
